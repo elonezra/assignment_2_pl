88fcf5986c1ce8a65904fdcdd2d371819f274b45
elonezra <elonezra1995@hotmail.com>
2024-02-14 12:04:21 +0200

    finished ex1

--- 0000000_00000000.rkt
+++ 0000000_00000000.rkt
@@ -1,1 +1,17 @@
 #lang pl 02
+#|
+Ex 1
+|#
+(: exp2 : Number -> Number)
+(define (exp2 x)
+  (* x x))
+(: sum-of-squares : (Listof Number) -> Number)
+(define (sum-of-squares lst)
+(foldl + 0 (map exp2 lst))
+  )
+
+(test (sum-of-squares '(1 2 3)) => 14)
+(test (sum-of-squares '(1 -2 3)) => 14)
+(test (sum-of-squares '(1 2 -3)) => 14)
+(test (sum-of-squares '(1/2 2 3)) => 53/4)
+(test (sum-of-squares '()) => 0)
