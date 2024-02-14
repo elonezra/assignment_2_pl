114859fd37c7546cfc43aa0ea3d77b2c062379bb
AviRahimov <rahimovavi3@gmail.com>
2024-02-14 20:17:21 +0200

    write solution for 2.b.1 and start 2.b.2

--- 0000000_00000000.rkt
+++ 0000000_00000000.rkt
@@ -24,7 +24,6 @@ Ex 1
 
 
 
-#|
 ; Question no. 2
 #|
 The `createPolynomial` function creates a polynomial function from a list of coefficients.
@@ -39,6 +38,7 @@ Example Usage:
 - `(define p (createPolynomial '(2 3 4)))` creates a polynomial function `p` that represents the polynomial 2 + 3x + 4x^2.
 - `(p 5)` evaluates the polynomial at `x = 5`.
 |#
+
 (: createPolynomial : (Listof Number) -> (Number -> Number))
 (define (createPolynomial coeffs)
   (: poly : (Listof Number) Number Integer Integer -> Number)
@@ -79,3 +79,65 @@ Example Usage:
 (test (p1 0) => 1)
 (test (p1 1) => 1)
 (test (p1 -1) => 1)
+
+#|
+Question no. 2.b.i
+
+The grammar: 
+ <PLANG> ::= {{poly <AEs>} {<AEs>}}
+ <AEs> ::= <AE> | <AE> <AEs> 
+ <AE> ::= <num>
+         |{+ <AE> <AE>}
+         |{- <AE> <AE>}
+         |{* <AE> <AE>}
+         |{/ <AE> <AE>}
+|#
+
+#|
+Question no. 2.b.ii
+|#
+(define-type PLANG
+  [Poly (Listof AE) (Listof AE)])
+
+(define-type AE
+  [Num Number] 
+  [Add AE AE] 
+  [Sub AE AE] 
+  [Mul AE AE] 
+  [Div AE AE])
+
+(: parse-sexpr : Sexpr -> AE) 
+ ;; to convert s-expressions into AEs
+(define (parse-sexpr sexpr)
+  (match sexpr 
+    [(number: n) (Num n)] 
+    [(list '+ lhs rhs) (Add (parse-sexpr lhs) 
+                            (parse-sexpr rhs))] 
+    [(list '- lhs rhs) (Sub (parse-sexpr lhs) 
+                            (parse-sexpr rhs))] 
+    [(list '* lhs rhs) (Mul (parse-sexpr lhs) 
+                            (parse-sexpr rhs))] 
+    [(list '/ lhs rhs) (Div (parse-sexpr lhs) 
+                            (parse-sexpr rhs))] 
+    [else (error 'parse-sexpr "bad syntax in ~s" sexpr)])) 
+
+(: parse : String -> PLANG) 
+ ;; parses a string containing a PLANG expression to a PLANG AST
+(define (parse str)
+  (let ([code (string->sexpr str)])
+    (match code
+      [(cons f r1)
+       (match f
+         [(cons 'poly r2) (Poly (list (append (parse-sexpr (first r2)) (parse (rest r2)))) (list (append (parse-sexpr (first r1)) (parse (rest r1)))))]
+      )]
+      )))
+
+; (list (list 'poly AE AE... AE) (list AE AE... AE))
+
+(test (parse "{{poly 1 2 3} {1 2 3}}")
+      => (Poly (list (Num 1) (Num 2) (Num 3))
+               (list (Num 1) (Num 2) (Num 3)))) 
+(test (parse "{{poly } {1 2} }")
+      =error> "parse: at least one coefficient is required in ((poly) (1 2))") 
+(test (parse "{{poly 1 2} {} }")
+      =error> "parse: at least one point is required in ((poly 1 2) ())")
